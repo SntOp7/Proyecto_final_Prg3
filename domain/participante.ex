385d0af74086dd4369598b5326b8078cd920896b
SntOp7 <santiago.ospinas@uqvirtual.edu.co>
2025-10-13 19:03:16 -0500

    Update participante.ex

--- domain/participante.ex
+++ domain/participante.ex
@@ -1,8 +1,8 @@
 defmodule Proyecto_final_prg3.Domain.Participante do
-  defstruct [:id, :nombre, :correo, :rol]
+  defstruct [:id, :nombre, :correo]
 
-  def nuevo(id, nombre, correo, rol) do
-    %_MODULE_{id: id, nombre: nombre, correo: correo, rol: rol}
+  def nuevo(id, nombre, correo) do
+    %__MODULE__{id: id, nombre: nombre, correo: correo}
   end
 
 end
