7f83d4f431a264196e5696767103704042773e56
Sharif Giraldo Obando <sharifgiraldo23@gmail.com>
2025-10-25 13:44:43 -0500

    feat: creación módulo equipo

---
+++ domain/equipo.ex
@@ -0,0 +1,15 @@
+defmodule Proyecto_final_prg3.Domain.Equipo do
+
+
+  defstruct [:id, :nombre, :descripcion, :categoria, :id_proyecto, :id_mentor, :participantes,
+  :fecha_creacion, :estado, :canal_chat_id, :puntaje, :historial]
+
+  def nuevo(id, nombre, descripcion, categoria, id_proyecto, id_mentor, participantes, fecha_creacion, estado, canal_chat_id, puntaje, historial) do
+    %__MODULE__{id: id, nombre: nombre, descripcion: descripcion, categoria: categoria, id_proyecto: id_proyecto,
+     id_mentor: id_mentor, participantes: participantes, fecha_creacion: fecha_creacion, estado: estado,
+      canal_chat_id: canal_chat_id, puntaje: puntaje, historial: historial}
+  end
+
+
+
+end
