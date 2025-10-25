d87f49881af6c26d830b7bc51884349dbb347962
JuanHG07 <juan.hernandezg@uqvirtual.edu.co>
2025-10-25 18:17:38 -0500

    Update mix.exs

--- mix.exs
+++ mix.exs
@@ -1,1 +1,26 @@
+defmodule ProyectoFinalPrg3.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :proyecto_final_prg3,
+      version: "0.1.0",
+      elixir: "~> 1.16",
+      start_permanent: Mix.env() == :prod,
+      deps: deps()
+    ]
+  end
+
+  def application do
+    [
+      extra_applications: [:logger]
+    ]
+  end
+
+  defp deps do
+    [
+      {:elixir_uuid, "~> 1.2"}
+    ]
+  end
+end
 
