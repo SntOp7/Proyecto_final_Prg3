3a903b7db32698cb65be904967744e6ea83c5d49
JuanHG07 <juan.hernandezg@uqvirtual.edu.co>
2025-10-26 13:40:48 -0500

    feat: Compile mix.exs
    Compilación de módulos y agrega dependencias al proyecto

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ProyectoFinalPrg3.MixProject do
   def project do
     [
       app: :proyecto_final_prg3,
-      version: "0.1.0",
+      version: "1.0.0",
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
       deps: deps()
@@ -13,12 +13,21 @@ defmodule ProyectoFinalPrg3.MixProject do
 
   def application do
     [
-      extra_applications: [:logger],
-      mod: {ProyectoFinalPrg3.Application, []}
+      mod: {ProyectoFinalPrg3.Application, []},
+      extra_applications: [:logger, :runtime_tools]
     ]
   end
 
   defp deps do
-    []
+    [
+      {:uuid, "~> 1.1"},             # UUIDs únicos
+      {:jason, "~> 1.4"},            # JSON encoding
+      {:phoenix_pubsub, "~> 2.1"},   # Comunicación PubSub
+      {:ex_doc, "~> 0.31", only: :dev, runtime: false},  # Documentación
+      {:ex_unit_notifier, "~> 1.2", only: :test},        # Notificaciones de test
+      {:excoveralls, "~> 0.17", only: :test},            # Cobertura de test
+      {:credo, "~> 1.7", only: [:dev, :test], runtime: false}, # Linter
+      {:dialyxir, "~> 1.4", only: [:dev], runtime: false}      # Analizador de tipos
+    ]
   end
 end
