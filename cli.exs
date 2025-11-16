94bb558d287c5dba502418eba710cb3066d32d65
Santiago Ospina Sanchez <santiago.ospinas@uqvirtual.edu.co>
2025-11-16 11:16:09 -0500

    feat: actualizacion de arquitectura del proyectp

---
+++ cli.exs
@@ -0,0 +1,23 @@
+Mix.Task.run("app.start")
+
+IO.puts("🎮 CLI lista (/help para ver comandos)\n")
+
+defmodule CLI.Main do
+  def loop do
+    case IO.gets("> ") do
+      nil ->
+        IO.puts("Saliendo...")
+
+      line ->
+        case ProyectoFinalPrg3.Adapters.CLI.CommandRouter.route(line) do
+          {:ok, msg} -> IO.puts(msg)
+          {:error, msg} -> IO.puts("❌ #{msg}")
+          other -> IO.inspect(other)
+        end
+
+        loop()
+    end
+  end
+end
+
+CLI.Main.loop()
