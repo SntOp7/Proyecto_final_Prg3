eac9c3577d4968849d54da5a12f6459a535f35fc
SntOp7 <santiago.ospinas@uqvirtual.edu.co>
2025-11-16 14:27:39 -0500

    Update mentor.ex

--- lib/domain/mentor.ex
+++ lib/domain/mentor.ex
@@ -12,6 +12,7 @@ defmodule ProyectoFinalPrg3.Domain.Mentor do
     :nombre,      # Nombre completo del mentor
     :correo,      # Correo electrónico
     :contrasena,  # Contraseña para autenticación
+    :rol,         # Rol (mentor)
     :especialidad # Área profesional o técnica del mentor
   ]
 
@@ -23,17 +24,19 @@ defmodule ProyectoFinalPrg3.Domain.Mentor do
     - `nombre`       → Nombre completo del mentor.
     - `correo`       → Correo electrónico.
     - `contrasena`   → Contraseña del mentor.
+    - 'rol'         -> rol
     - `especialidad` → Área de experiencia profesional.
 
   ## Ejemplo:
-      iex> Mentor.nuevo("m1", "Ana Torres", "ana@hackathon.com", "1234", "IA")
+      iex> Mentor.nuevo("m1", "Ana Torres", "ana@hackathon.com", "1234", "mentor" "IA")
   """
-  def nuevo(id, nombre, correo, contrasena, especialidad) do
+  def nuevo(id, nombre, correo, contrasena, rol, especialidad) do
     %__MODULE__{
       id: id,
       nombre: nombre,
       correo: correo,
       contrasena: contrasena,
+      rol: rol,
       especialidad: especialidad
     }
   end
